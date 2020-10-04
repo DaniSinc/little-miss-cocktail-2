7614b176c143c21129b7ca38cf8cd8e1d84a7faa
Daniela Sinclair <dasfluid@gmail.com>
2020-10-04 16:18:26 +0100

    JSON parse to seed 100 ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+file = open(url).read
+cocktails = JSON.parse(file)
+drinks = cocktails["drinks"]
+drinks.each do |drink|
+    Ingredient.create(name: drink["strIngredient1"])
+end
