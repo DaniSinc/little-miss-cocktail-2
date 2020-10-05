a230ce994784590bd3c5d707cc9ea05bca363144
Daniela Sinclair <dasfluid@gmail.com>
2020-10-05 08:54:05 +0100

    Fixed Cloudinary problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,12 @@
 require 'open-uri'
 require 'json'
 
+puts "Destroying cocktails"
+Cocktail.destroy_all
+puts "Destroying ingredients"
+Ingredient.destroy_all
+
+puts "Creating Ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 file = open(url).read
 cocktails = JSON.parse(file)
@@ -15,3 +21,10 @@ drinks = cocktails["drinks"]
 drinks.each do |drink|
     Ingredient.create(name: drink["strIngredient1"])
 end
+
+puts "Creating Cocktails"
+file = URI.open('https://images.unsplash.com/photo-1598373187432-c1ff06874ce8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
+cocktail = Cocktail.new(name: 'Madame Butterfly')
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+
