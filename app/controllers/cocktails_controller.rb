a6bf4deb736c492bcca83e1b21363bae89a8f0c4
Daniela Sinclair <dasfluid@gmail.com>
2020-10-06 12:59:22 +0100

    Added delete cocktail feature

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_index_path
+  end
+
   private
 
   def cocktail_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get 'cocktails/index'
   get 'cocktails/new'
   get 'cocktails/show'
+  delete 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
